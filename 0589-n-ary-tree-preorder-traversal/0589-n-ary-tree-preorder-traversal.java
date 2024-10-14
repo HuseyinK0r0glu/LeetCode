0f43f454a4037a6383fab7b7e7b7d7f064bbd4d2
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-10-14 22:43:27 +0300

    Time: 0 ms (100.00%), Space: 44 MB (99.49%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        dfs(root,list);
+        
+        return list;
+    }
+    
+    public void dfs(Node node ,List<Integer> list){
+        
+        if(node == null) return;
+        
+        List<Node> children = node.children;
+        
+        int n = children.size();
+        
+        list.add(node.val);
+        
+        for(int i = 0;i<n;i++){
+            dfs(children.get(i),list);
+        }
+        
+    }
+}

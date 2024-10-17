4ac8e1106419b2a47a0117b3b6519408b5b178bb
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-10-17 13:46:44 +0300

    Time: 1 ms (99.94%), Space: 45.6 MB (44.12%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        // greedy 
+        
+        int goal = nums.length - 1; 
+        
+        for(int i = nums.length-1;i>=0;i--){
+            
+            if(i + nums[i] >= goal) goal = i;
+            
+        }
+        
+        return goal == 0;
+        
+    }
+}

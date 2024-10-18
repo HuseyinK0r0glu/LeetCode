53d527309235e45691548248bc903a9e20805144
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-10-18 22:17:15 +0300

    Time: 4 ms (76.23%), Space: 44.2 MB (67.12%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,6 +11,8 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         
+        /*
+        // brute force
         List<Integer> result = new ArrayList<>();
         
         for(int i = 0;i<lists.length;i++){
@@ -28,6 +30,29 @@ class Solution {
         for(int i = 0;i<result.size();i++){
             current.next = new ListNode(result.get(i));
             current = current.next;
+        }
+        
+        return res.next;
+        */
+        
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        
+        ListNode res = new ListNode(0);
+        ListNode current = res;
+        
+        for(ListNode node : lists){
+            if(node != null) pq.add(node);
+        }
+        
+        while(!pq.isEmpty()){
+            
+            ListNode smallestNode = pq.poll();
+            
+            current.next = smallestNode;
+            current = current.next;
+            
+            if(smallestNode.next != null) pq.add(smallestNode.next);
+            
         }
         
         return res.next;

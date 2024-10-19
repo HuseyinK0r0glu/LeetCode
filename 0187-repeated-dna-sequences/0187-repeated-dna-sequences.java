7c610f2a8c3b2694e334a294128cdc8d1d397b95
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-10-19 12:08:18 +0300

    Time: 21 ms (50.43%), Space: 52.4 MB (84.28%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,6 +1,8 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         
+        /*
+        // hashmap
         List<String> list = new ArrayList<>();
         
         Map<String,Integer> map = new HashMap<>();
@@ -19,5 +21,23 @@ class Solution {
         }
         
         return list;
+        */
+        // set
+        
+        Set<String> result = new HashSet<>();
+        Set<String> set = new HashSet<>();
+        
+        int left = 0;
+        for(int right = 10;right<=s.length();right++){
+            if(set.contains(s.substring(left,right))){
+                result.add(s.substring(left,right));
+            }else {
+                set.add(s.substring(left,right));
+            }
+            left++;
+        }
+        
+        return new ArrayList<>(result);
+        
     }
 }

ac8cb7930c8fd24db782a1ce48c833995fb57153
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-11-02 13:59:50 +0300

    Time: 0 ms (100.00%), Space: 40.4 MB (97.32%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -1,6 +1,10 @@
 class Solution {
     public char findKthBit(int n, int k) {
         
+        return helper(n,k);
+        
+        /*
+        // brute force
         String s = "0";
     
         while(n>0){
@@ -11,9 +15,35 @@ class Solution {
         }
         
         return s.charAt(k-1);
+        */
+    }
+    
+    public char helper(int n ,int k){
+        
+        if(n == 1) return '0';
+        
+        int length = (1 << n)-1;
+        int mid = length/2 + 1;
+        
+        if(mid == k){
+            return '1';
+        }else if(k<mid){
+            return helper(n-1,k);
+        }else{
+            int mirrorK = length - k + 1;
+            char res = helper(n-1,mirrorK);
+            return inverse(res);
+        }
         
     }
     
+    public char inverse(char bit){
+        return bit == '0' ? '1' : '0';
+    }
+    
+    
+    /*
+    // brute force 
     public String reverse(String s){
         
         StringBuilder sb = new StringBuilder();
@@ -40,5 +70,6 @@ class Solution {
         return sb.toString();
         
     }
+    */
     
 }

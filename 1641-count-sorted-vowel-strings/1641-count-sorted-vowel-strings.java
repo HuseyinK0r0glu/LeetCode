f92aa36603ab22e7bd0763bbb4bd02a8f3bf8649
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-11-15 16:13:52 +0300

    Time: 518 ms (5.26%), Space: 40.2 MB (72.33%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int countVowelStrings(int n) {
+        
+        //return (n+1) * (n+2) * (n+3) * (n+4) / 24;
+        
+//         backtrack
+        String[] vowels = {"a", "e", "i", "o", "u"};
+
+        int[] count = {0};
+        backtrack(vowels,new StringBuilder(),0,n,count);
+
+        return count[0];
+    }
+
+    public static void backtrack(String[] vowels,StringBuilder sb,int index,int n,int[] count) {
+
+        if(sb.length() == n){
+            count[0]++;
+            return;
+        }
+
+        for(int i = index;i<vowels.length;i++){
+            sb.append(vowels[i]);
+            backtrack(vowels,sb,i,n,count);
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+    }
+}

780e9824518fc134a637a50632355a6448557ccb
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-11-16 22:33:26 +0300

    Time: 0 ms (100.00%), Space: 40.7 MB (14.25%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int sumOfTheDigitsOfHarshadNumber(int x) {
+
+        int temp = x;
+        int sum = 0;
+
+        while(temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        if(x % sum == 0) {
+            return sum;
+        }
+        return -1;
+
+    }
+}

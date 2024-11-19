11b49ce19d975dac6a572504ee5758e2b13cf991
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-11-19 13:32:21 +0300

    Time: 1 ms (100.00%), Space: 42.4 MB (84.79%) - LeetHub

---
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static String getEncryptedString(String s, int k) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0;i<s.length();i++){
+            sb.append(s.charAt((i+k)%s.length()));
+        }
+
+        return sb.toString();
+    }
+}

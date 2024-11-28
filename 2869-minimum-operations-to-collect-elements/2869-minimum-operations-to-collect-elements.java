129759a8c4e94ea273baa10241cd47a1dcf194f3
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-11-28 12:07:49 +0300

    Time: 1 ms (97.09%), Space: 42.4 MB (10.03%) - LeetHub

---
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(List<Integer> nums, int k) {
+
+        Set<Integer> set = new HashSet<>();
+
+        int operations = 0;
+
+        for(int i = nums.size() -1 ;i>=0;i--){
+            operations++;
+            if(nums.get(i) <= k){
+                set.add(nums.get(i));
+            }
+            if(set.size() == k) break;
+        }
+
+        return operations;
+    }
+}

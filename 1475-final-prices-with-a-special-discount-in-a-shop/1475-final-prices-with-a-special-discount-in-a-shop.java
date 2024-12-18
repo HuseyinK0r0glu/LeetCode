e8cadebfa9cba6fa287bc728772243c33c17c004
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-12-18 18:59:01 +0300

    Time: 1 ms (98.90%), Space: 43.8 MB (38.43%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,17 +1,34 @@
 class Solution {
     public static int[] finalPrices(int[] prices) {
-        int[] res = new int[prices.length];
-
-        for(int i = 0; i < prices.length; i++){
-            res[i] = prices[i];
-            for(int j = i+1; j < prices.length; j++){
-                if(prices[i] >= prices[j]){
-                    res[i] = prices[i] - prices[j];
+        
+        
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = i + 1; j < prices.length; j++) {
+                if(prices[i] >= prices[j]) {
+                    prices[i] = prices[i] - prices[j];
                     break;
                 }
             }
         }
 
-        return res;
+        return prices;
+    
+        
+        
+        
+        // previous solution
+//         int[] res = new int[prices.length];
+
+//         for(int i = 0; i < prices.length; i++){
+//             res[i] = prices[i];
+//             for(int j = i+1; j < prices.length; j++){
+//                 if(prices[i] >= prices[j]){
+//                     res[i] = prices[i] - prices[j];
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return res;
     }
 }

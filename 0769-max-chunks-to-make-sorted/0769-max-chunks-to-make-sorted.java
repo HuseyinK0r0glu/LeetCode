0829ee2da3debdfa0d59926fce43a938b0d1afc0
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2024-12-19 23:16:00 +0300

    Time: 0 ms (100.00%), Space: 41.3 MB (6.63%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < arr.length; i++){
+            
+            if(stack.isEmpty() || arr[stack.peek()] < arr[i]){
+                stack.push(i);
+            }else {
+                int max = stack.peek();
+                while(!stack.isEmpty() && arr[stack.peek()] > arr[i]){
+                    stack.pop();
+                }
+                stack.push(max);
+            }
+        }
+
+        return stack.size();
+    }
+
+}

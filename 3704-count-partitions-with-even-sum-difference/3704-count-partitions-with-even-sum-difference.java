eec0cd9d48aa7a907e2b3f1d6bbfb9ae2a89199f
HuseyinKoroglu <168372337+HuseyinK0r0glu@users.noreply.github.com>
2025-02-25 12:25:46 +0300

    Time: 0 ms (100%), Space: 41.9 MB (83.52%) - LeetHub

--- 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.java
@@ -1,33 +1,54 @@
 class Solution {
     public int countPartitions(int[] nums) {
 
-        int[] prefixSumRight = new int[nums.length];
-        int[] prefixSumLeft = new int[nums.length];
-
-        int sum = 0;
-
-        for(int i = 0;i<nums.length;i++){
-            sum += nums[i];
-            prefixSumLeft[i] = sum;
-        }
-
-        sum = 0;
-
-        for(int i = nums.length-1;i>=0;i--){
-            sum += nums[i];
-            prefixSumRight[i] = sum;
+        int total = 0;
+        for(int num : nums){
+            total += num;
         }
 
         int count = 0;
-
-        for(int i = 0;i<nums.length - 1;i++){
-            int leftSubArray = prefixSumLeft[i];
-            int rightSubArray = prefixSumRight[i] - nums[i];
-            if((rightSubArray - leftSubArray) % 2 == 0){
+        int sum = 0;
+        
+        for(int i = 0;i<nums.length-1;i++){
+            sum += nums[i];
+            int rightSum = total - sum;
+            
+            if((rightSum - sum) % 2 == 0){
                 count++;
             }
         }
 
         return count;
+        
+// WITH TWO PREFIX ARRAYS
+//        int[] prefixSumRight = new int[nums.length];
+//        int[] prefixSumLeft = new int[nums.length];
+//
+//        int sum = 0;
+//
+//        for(int i = 0;i<nums.length;i++){
+//            sum += nums[i];
+//            prefixSumLeft[i] = sum;
+//        }
+//
+//        sum = 0;
+//
+//        for(int i = nums.length-1;i>=0;i--){
+//            sum += nums[i];
+//            prefixSumRight[i] = sum;
+//        }
+//
+//        int count = 0;
+//
+//
+//        for(int i = 0;i<nums.length - 1;i++){
+//            int leftSubArray = prefixSumLeft[i];
+//            int rightSubArray = prefixSumRight[i+1];
+//            if((rightSubArray - leftSubArray) % 2 == 0){
+//                count++;
+//            }
+//        }
+//
+//        return count;
     }
 }
